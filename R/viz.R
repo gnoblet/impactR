eb297684ddd13796d326b6621dfa6567f98ed040
gnoblet <gnoblet@zaclys.net>
2022-04-24 14:45:34 +0200

    Combine plots.R and maps.R in viz.R

--- R/viz.R
+++ R/viz.R
@@ -125,18 +125,18 @@ theme_hist_simple_reach <- function(family = "Leelawadee UI") {
 #' @details This function needs to be modified to add colors
 #'
 #' @export
-reach_cols <- function(...) {
+reach_cols <- function(..., unnamed = T) {
   cols <- c(...)
 
   reach_colors <- c(white        = "#FFFFFF",
-                    black        = "#00000",
+                    black        = "#000000",
                     main_grey    = "#58585A",
                     main_red     = "#EE5859",
                     main_lt_grey = "#c7c8ca",
                     main_beige   = "#D2CBB8",
-                    iroise_1     = "DFECEF",
-                    iroise_2     = "B1D7E0",
-                    iroise_3     = "699DA3",
+                    iroise_1     = "#DFECEF",
+                    iroise_2     = "#B1D7E0",
+                    iroise_3     = "#699DA3",
                     iroise_4     = "#236A7A",
                     iroise_5     = "#0C3842",
                     red_main_1   = "#AE2829",
@@ -148,7 +148,7 @@ reach_cols <- function(...) {
                     red_alt_2    = "#c0474a",
                     red_alt_3    = "#ee5859",
                     red_alt_4    = "#f49695",
-                    red_alt_5    ="#f8d6d6",
+                    red_alt_5    = "#f8d6d6",
                     red_alt_na   = "#f8f4f4",
                     lt_grey_1    = "#C6C6C6",
                     lt_grey_2    = "#818183",
@@ -173,9 +173,15 @@ reach_cols <- function(...) {
                     dk_green     = "#526450")
 
   if (is.null(cols))
-    return (reach_colors)
+    cols_to_return <- reach_colors
 
-  reach_colors[cols]
+  cols_to_return <- reach_colors[cols]
+
+  if(unnamed){
+    cols_to_return <- unname(cols_to_return)
+  }
+
+  return(cols_to_return)
 }
 
 
@@ -193,7 +199,7 @@ reach_cols <- function(...) {
 #' @return A color palette
 #'
 #' @export
-reach_pal <- function(palette = "main", reverse = FALSE, color_ramp_palette = T, ...) {
+reach_pal <- function(palette = "main", reverse = FALSE, color_ramp_palette = F, ...) {
 
 
   reach_palettes <- list(
@@ -204,6 +210,7 @@ reach_pal <- function(palette = "main", reverse = FALSE, color_ramp_palette = T,
     `two_dots_flashy` = reach_cols("two_dots_flashy_1", "two_dots_flashy_2"),
     `red_main`        = reach_cols("red_main_1", "red_main_2", "red_main_3", "red_main_4", "red_main_5"),
     `red_alt`         = reach_cols("red_alt_1", "red_alt_2", "red_alt_3", "red_alt_4", "red_alt_5"),
+    `iroise`          = reach_cols("iroise_1", "iroise_2", "iroise_3", "iroise_4", "iroise_5"),
     `discrete_6`      = reach_cols("dk_grey", "red_main_1", "main_beige", "red_main_2", "lt_grey_2", "red_4")
   )
 
@@ -238,7 +245,7 @@ scale_color_reach  <- function(palette = "main", discrete = TRUE, reverse = FALS
 
   rlang::check_installed("ggplot2", reason = "Package \"ggplot2\" needed for `scale_color_reach()` to work. Please install it.")
 
-  pal <- reach_pal(palette = palette, reverse = reverse)
+  pal <- reach_pal(palette = palette, reverse = reverse, color_ramp_palette = T)
 
   if (discrete) {
     ggplot2::discrete_scale("colour", paste0("reach_", palette), palette = pal, ...)
@@ -261,7 +268,7 @@ scale_fill_reach <- function(palette = "main", discrete = TRUE, reverse = FALSE,
 
   rlang::check_installed("ggplot2", reason = "Package \"ggplot2\" needed for `scale_fill_reach()` to work. Please install it.")
 
-  pal <- reach_pal(palette = palette, reverse = reverse)
+  pal <- reach_pal(palette = palette, reverse = reverse, , color_ramp_palette = T)
 
   if (discrete) {
     ggplot2::discrete_scale("fill", paste0("reach_", palette), palette = pal, ...)
@@ -611,4 +618,105 @@ reach_hist_bar <- function(.tbl,
   return(g)
 }
 
+
+
+
+#' @title Some  REACH basic `tmap` options
+#'
+#' @param check_and_fix Param check.and.fix from `tmap::tmap_options()`
+#' @param fontfamily Param fontfamily from `tmap::tmap_options()`
+#' @param fontface Param fontface from `tmap::tmap_options()`
+#' @param legend_title_size Param legend.title.size from `tmap::tmap_options()`
+#' @param legend_text_size Param legend.text.size from `tmap::tmap_options()`
+#' @param legend_text_fontface Param legend legend.text.fontface from `tmap::tmap_options()`
+#' @param legend_width Param legend.width from `tmap::tmap_options()`
+#' @param legend_position Param legend.position from `tmap::tmap_options()`
+#' @param legend_frame Param legend.frame from `tmap::tmap_options()`
+#' @param output_dpi Param output.dpi from `tmap::tmap_options()`
+#' @param frame Frame colors using `impactR::reach_cols()`
+#' @param inner_margins Param inner.margins from `tmap::tmap_options()`
+#' @param outer_margins Param outer.margins from `tmap::tmap_options()`
+#' @param asp Param asp from `tmap::tmap_options()`
+#' @param ... Other parameters to pass to `tmap::tmap_options()`
+#'
+#' @description Give some reach colors and font for maps
+#'
+#' @return Load tmap options globally
+#'
+#' @export
+tmap_options_reach <- function(check_and_fix = T,
+                               fontfamily = "Leelawadee UI",
+                               fontface = 1,
+                               legend_title_size = 0.9,
+                               legend_text_size = 0.8,
+                               legend_text_fontface = 1,
+                               legend_width = 0.9,
+                               legend_position = c("right", 0.14),
+                               legend_frame = impactR::reach_colors()$reach_dk_grey,
+                               output_dpi = 500,
+                               frame = impactR::reach_cols("main_grey"),
+                               inner_margins = c(0,0,0,0),
+                               outer_margins= c(0,0,0,0),
+                               asp = 0,
+                               ...){
+
+  rlang::check_installed("tmap", reason = "Package \"tmap\" needed for `tmap_options_reach()` to work. Please install it.")
+
+  tmap::tmap_options(check.and.fix = check_and_fix,
+                     fontfamily = fontfamily,
+                     fontface = fontface,
+                     legend.title.size = legend_title_size,
+                     legend.text.size = legend_text_size,
+                     legend.text.fontface = legend_text_fontface,
+                     legend.width =  legend_width,
+                     legend.position = legend_position,
+                     legend.frame = legend_frame,
+                     output.dpi = output_dpi,
+                     frame = frame,
+                     inner.margins = inner_margins,
+                     outer.margins = outer_margins,
+                     asp = 0,
+                     ...)
+}
+
+
+#' @title Bbbox buffer
+#'
+#' @param sf_obj A `sf` object
+#' @param buffer A buffer, either one value or a vector of 4 values (left, bottom, right, top). Default to 0.
+#'
+#' @return A bbox with a buffer
+#'
+#' @export
+buffer_bbox <- function(sf_obj, buffer = 0){
+
+  rlang::check_installed("sf", reason = "Package \"sf\" needed for `buffer_bbox()` to work. Please install it.")
+
+
+  if (!(length(buffer) %in% c(1,4)) | !is.numeric(buffer)) stop("Please provide a numeric buffer of length 1 or 4.")
+
+  bbox <- sf::st_bbox(sf_obj)
+  xrange <- bbox$xmax - bbox$xmin # range of x values
+  yrange <- bbox$ymax - bbox$ymin # range of y values
+
+
+  bbox_with_buffer <- if (length(buffer) == 1) {
+    c(
+      bbox[1] - (buffer * xrange), # xmin - left
+      bbox[2] - (buffer * yrange), # ymin - bottom
+      bbox[3] + (buffer * xrange), # xmax - right
+      bbox[4] + (buffer * yrange) # ymax - top
+    )
+  } else if (length(buffer) == 4) {
+    c(
+      bbox[1] - (buffer[1] * xrange), # xmin - left
+      bbox[2] - (buffer[2] * yrange), # ymin - bottom
+      bbox[3] + (buffer[3] * xrange), # xmax - right
+      bbox[4] + (buffer[4] * yrange) # ymax - top
+    )
+  } else {
+    print("Missed something while writing the funtion.")
+  }
+
+}
 
