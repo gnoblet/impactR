3fe6e37e92e077491c056aee2f7439b2889aed29
gnoblet <gnoblet@zaclys.net>
2022-04-26 23:32:23 +0200

    Fixes #35

--- R/clean.R
+++ R/clean.R
@@ -27,80 +27,13 @@ log_names_en_fr  <- function(.tbl){
 
 
 
-#' @title  Check cleaning log
-#' @param log A cleaning log
-#' @param .tbl Data to clean
-#'
-#' @details Names should be the English version. If necessary, please use [impactR::log_names_fr_en] beforehand.
-#'
-#' @family functions to check logs and check lists
-#'
-#' @importFrom rlang .data
-#'
-#' @export
-check_cleaning_log <- function(log, .tbl){
-  en_names <- c("id_check", "question_name", "question_label", "why", "feedback", "action", "old_value", "new_value", "type", "other_parent_question", "other_old_value", "other_new_value", "checkdate" )
-
-  # Check if the column names are the right names
-  are_cols_in <- !(en_names %in% colnames(log))
-
-  if(sum(are_cols_in) >= 1) {
-    stop("The following columns are missing and required: ", paste(en_names[are_cols_in], collapse = ", "), ".")
-  }
-
-  # Check if all id_check are there
-  if(sum(is.na(log$id_check)) >= 1){
-    stop("There are missing 'id_check' values.")
-  }
-
-  # Check if all types are there
-  if(sum(is.na(log$type)) >= 1){
-    stop("There are missing 'type' values.")
-  }
-
-  # Check if all question_names are there
-  if(sum(is.na(log$question_name) >= 1)){
-    stop("There are missing 'question_name' values.")
-  }
-
-  # Check if the types are one of the right ones
-  types <- sum(stringr::str_count(log$type, pattern = "character|double"))
-
-  if(types < nrow(log)){
-    stop("There are wrong types in column 'type'. It should be either 'character' or 'double'.")
-  }
-
-  # Check if the actions are one of the right ones
-  actions <- sum(stringr::str_count(log$action, pattern = "keep|modify|remove|check|duplicate"))
-
-  if(actions < nrow(log)){
-    stop("There are wrong actions in column 'action'. It should be either 'keep', 'modify', 'remove' or 'check'.")
-  }
-
-  # Check if question_name that needs a modification belongs to the rawdata
-  are_cols_in <- !(log |> dplyr::filter(.data$action == "modify") |> dplyr::pull(.data$question_name) %in% colnames(.tbl))
-
-  if(sum(are_cols_in) >= 1){
-    stop("The following 'question_name' values does not exist in the rawdata column names: ", paste(log$question_name[are_cols_in], collapse = ", "), ".")
-  }
-
-  # To be added :
-  # - check types of the question name in the log against its type in the rawdata
-  # - check that new value is not the same as the old value
-  # - check that new value is not
-
-  return(NULL)
-}
-
-
-
 #' @title Delete from log
 #'
 #' @param .tbl A tibble
-#' @param log A deletion log, which contains a column "action".
+#' @param log A deletion log, which contains a column "action"
 #' @param id_col The column id, usually "uuid"
 #'
-#' @description Removes surveys from .tbl where column "action" in the log is set to "remove".
+#' @description Removes surveys from .tbl where column "action" in the log is set to "remove"
 #'
 #' @return A lot of removal
 #'
---
+++ R/utils_df.R
@@ -0,0 +1,243 @@
+# Helo utils!
+
+# All utils to work on dataframes/tibbles
+# Recode
+# Recode NAs
+# Rename many columns
+# Left join several dataframes
+# Remove cols from df1 that exist ini df2
+# Group split
+
+
+#' @title Remove cols from tibble
+#'
+#' @param .tbl A tibble
+#' @param ... Column names
+#'
+#' @return A tibble with columns removed
+#'
+#' @export
+rm_cols <- function(.tbl, ...) {
+
+  quoted_cols <- purrr::map_chr(rlang::enquos(...), rlang::as_name)
+  if_not_in_stop(.tbl, quoted_cols, ".tbl", arg = "...")
+
+  .tbl |>
+    dplyr::select(- dplyr::all_of(quoted_cols))
+}
+
+
+
+#' @title Replace values of some tibble's columns to one value
+#'
+#' @param .tbl A tibble
+#' @param values A vector of values
+#' @param to_value A value
+#' @param ... Column names
+#'
+#' @return A tibble with recoded values to one value
+#'
+#' @details If the column type is a character and the replacement a numeric, then the numeric is coerced to a character. If the column type is a numeric and the replacement is a character, then the column is coerced to character. NAs will remains NAs of the right type.
+#'
+#' @export
+rec_values <- function(.tbl, values, to_value, ...){
+
+  quoted_cols <- purrr::map_chr(rlang::enquos(...), rlang::as_name)
+  if_not_in_stop(.tbl, quoted_cols, ".tbl", arg = "...")
+
+  .tbl |>
+    dplyr::mutate(dplyr::across(.cols = dplyr::all_of(quoted_cols), ~ replace(., . %in% values, to_value)))
+}
+
+
+
+#' @title Replace many types of NAs of a tibble to NA
+#'
+#' @param .tbl A tibble
+#' @param ... Column names
+#'
+#' @return A tibble with one type of NAs
+#'
+#' @export
+rec_na <- function(.tbl, ...){
+  nas <- c(NULL, "NULL", "N/A", "n/a", 999, 998, 888, " ", Inf, -Inf,
+           9999, "(vide)", "(empty)", "d/m", "", "NA", "na", "", " ",
+           NaN, "NaN", "Na", -999, -9999, -998, -888)
+
+  .tbl |>
+    rec_values(nas, NA, ...)
+}
+
+
+
+#' @title Rename columns
+#'
+#' @param .tbl A tible
+#' @param old_cols A vector of old quoted variable names
+#' @param new_cols A vector of new quoted variable names
+#'
+#' @return Updated tibble with new names
+#'
+#' @export
+rename_cols <- function(.tbl, old_cols, new_cols){
+
+  if_not_in_stop(.tbl, old_cols, ".tbl", "old_cols")
+
+  if (length(old_cols) != length(new_cols)) {
+    abort_bad_argument("new_cols", "be the same length as `old_cols`")
+  }
+
+  up_tbl <- .tbl |>
+    dplyr::rename_with(~ new_cols, .cols = dplyr::all_of(old_cols))
+
+  return(up_tbl)
+}
+
+
+
+
+#' @title Left join a few tibble from a list
+#'
+#' @param list A list of tibbles
+#' @param ... Grouping columns
+#'
+#' @return A left-joined tibble
+#'
+#' @export
+left_joints <- function(list, ...) {
+
+  quoted_cols <- purrr::map_chr(rlang::enquos(...), rlang::as_name)
+
+  purrr::imap(list, ~ if_not_in_stop(.x, quoted_cols, .y, arg = "..."))
+
+  joined <- list |>
+    purrr::reduce(
+      dplyr::left_join,
+      by = quoted_cols
+      )
+
+  return(joined)
+}
+
+
+
+#' @title Remove cols from a tibble that exists in another tibble
+#'
+#' @param tibble_a A tibble to remove columns from
+#' @param tibble_b A tibble to extract columns names from
+#' @param ... Columns to keep in tibble.
+#'
+#' @return A tibble with some columns removed
+#'
+#' @export
+diff_tibbles <- function(tibble_a, tibble_b, ...){
+
+  #-------- Checks
+
+  quoted_cols <- purrr::map_chr(rlang::enquos(...), rlang::as_name)
+  if_not_in_stop(tibble_b, quoted_cols, "tibble_b", arg = "cols")
+
+  #-------- Make the diff
+
+  cols_to_remove <- tibble_b |>
+    dplyr::select(- dplyr::all_of(quoted_cols)) |>
+    colnames()
+
+  removed <- tibble_a |>
+    dplyr::select(- dplyr::any_of(cols_to_remove))
+
+  return(removed)
+}
+
+
+
+#' @title Named group split
+#'
+#' @param .tbl A tibble of data
+#' @param group Column to split group by
+#'
+#' @return  A split and named list of tibbles
+#'
+#' @export
+named_group_split <- function (.tbl, group){
+
+  group_name <- rlang::as_name(rlang::enquo(group))
+  if_not_in_stop(.tbl, group_name, ".tbl", "group")
+
+  names <- .tbl |>
+    dplyr::group_by({{ group }}) |>
+    dplyr::group_keys() |>
+    dplyr::pull({{ group }})
+
+  split <- .tbl |>
+    dplyr::group_split({{ group }}) |>
+    purrr::set_names(names)
+
+  return(split)
+}
+
+
+
+
+#' @title Calculate the duration of a survey
+#'
+#' @param .tbl A tibble
+#' @param start Start column name
+#' @param end End column name
+#'
+#' @details Note: it is necessary to have 'start' and 'end' columns
+#'
+#' @return A tibble with three new colums, including the duration of survey in minutes
+#'
+#' @export
+svy_duration <- function(.tbl, start, end){
+
+  duration <- .tbl  |>
+    dplyr::mutate(
+      start = lubridate::ymd_hms({{ start }}, truncated = 1),
+      end = lubridate::ymd_hms({{ end }}, truncated = 1),
+      survey_duration = round(difftime({{ end }},  start, units = "mins")) |>  as.double()
+    )
+
+  return(duration)
+}
+
+
+#' @title Check in-between surveys time
+#'
+#' @param .tbl A tibble
+#' @param start Start column (unquoted)
+#' @param end End column (unquoted)
+#' @param ... The columns to group by, usually a locality or an enumerator id
+#' @param new_colname The new column name of the time difference
+#' @details Note: it is necessary to have 'start' and 'end' columns with no NA
+#'
+#' @return A tibble with ... removed
+#'
+#' @export
+svy_difftime <- function(.tbl, start, end, ..., new_colname = "survey_difftime"){
+
+  #-------- Checks
+
+  # Check for start in .tbl
+  start_name <- rlang::enquo(start) |> rlang::as_name()
+  if_not_in_stop(.tbl, start_name, ".tbl", "start")
+
+  # Check for start in .tbl
+  end_name <- rlang::enquo(end()) |> rlang::as_name()
+  if_not_in_stop(.tbl, end_name, ".tbl", "end")
+
+  # Check for ... in .tbl
+  cols_to_keep <- purrr::map_chr(rlang::enquos(...), rlang::as_name)
+  if_not_in_stop(.tbl, cols_to_keep, ".tbl", arg = "...")
+
+  #-------- Add difftime column
+
+  diff_time <- .tbl |>
+    dplyr::arrange(..., {{ start }}) |>
+    dplyr::group_by(...) |>
+    dplyr::mutate("{new_colname}" := difftime({{ start }}, dplyr::lag({{ end }}), units = "mins") |> round() |> as.double()) |>
+    dplyr::ungroup()
+
+    return(diff_time)
+}
